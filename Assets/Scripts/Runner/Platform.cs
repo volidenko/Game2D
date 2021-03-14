14167f2cc6274c188deabe1b588a97145e53ec21
volidenko <volidenko@i.ua>
2021-03-15 01:56:18 +0200

    comm

---
+++ Assets/Scripts/Runner/Platform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour{
+    private PlatformManager _platformManager;
+    public float lOffset;
+
+    private void OnEnable(){
+        _platformManager=GameObject.FindObjectOfType<PlatformManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnBecameInvisible(){
+        _platformManager.RecyclePlatform(this.GameObject, lOffset);
+    }
+}
---
+++ Assets/Scripts/Runner/PlatformManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour{
+    [SerializeField]	
+    private GameObject[] _platformPrefabs;	
+    private float offset=0f;
+
+    // Start is called before the first frame update
+    void Start () {				
+        for (int i = 0; i< _platformPrefabs.Length;i++){
+            Instantiate(_platformPrefabs[i],new Vector3(0,0,offset),Quaternion.Euler(0,0,0));
+            offset+=100f;
+        }
+    }
+
+    public void RecyclePlatform(GameObject platform, float loffset){
+        platform.transform.position= new Vector3(0,0,offset);		
+        offset+=loffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
